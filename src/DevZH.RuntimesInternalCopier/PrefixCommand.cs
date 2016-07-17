5ef4413b87d46ae615c1460033d07bf06b17062a
杜晓宇 <noliar@qq.com>
2016-07-18 00:09:23 +0800

    initial

---
+++ src/DevZH.RuntimesInternalCopier/PrefixCommand.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace DevZH.RuntimesInternalCopier
+{
+    public class PrefixCommand
+    {
+        public string Prefix { get; }
+
+        public PrefixCommand(string prefix)
+        {
+            Prefix = prefix;
+        }
+
+        public int Run()
+        {
+            try
+            {
+                var packagesFolder = Environment.GetEnvironmentVariable("NUGET_PACKAGES");
+
+                if (string.IsNullOrEmpty(packagesFolder))
+                {
+                    packagesFolder = Path.Combine(GetHome(), ".nuget", "packages");
+                }
+
+                packagesFolder = Environment.ExpandEnvironmentVariables(packagesFolder);
+
+                var lockJson = JObject.Parse(File.ReadAllText("project.lock.json"));
+
+                foreach (var libuvLib in lockJson["libraries"].OfType<JProperty>().Where(
+                    p => p.Name.StartsWith(Prefix, StringComparison.Ordinal)))
+                {
+                    foreach (var filePath in libuvLib.Value["files"].Select(v => v.Value<string>()))
+                    {
+                        if (filePath.ToString().StartsWith("runtimes/", StringComparison.Ordinal))
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                            File.Copy(Path.Combine(packagesFolder, libuvLib.Name, filePath), filePath, overwrite: true);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+            return 0;
+        }
+
+        // Copied from DNX's DnuEnvironment.cs
+        private static string GetHome()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return Environment.GetEnvironmentVariable("USERPROFILE") ??
+                    Environment.GetEnvironmentVariable("HOMEDRIVE") + Environment.GetEnvironmentVariable("HOMEPATH");
+            }
+            else
+            {
+                var home = Environment.GetEnvironmentVariable("HOME");
+
+                if (string.IsNullOrEmpty(home))
+                {
+                    throw new Exception("Home directory not found. The HOME environment variable is not set.");
+                }
+
+                return home;
+            }
+        }
+    }
+}
--- src/DevZH.RuntimesInternalCopier/Program.cs
+++ src/DevZH.RuntimesInternalCopier/Program.cs
@@ -1,14 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Microsoft.Extensions.CommandLineUtils;
+using Newtonsoft.Json.Linq;
 
 namespace DevZH.RuntimesInternalCopier
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            var app = new CommandLineApplication
+            {
+                Name = "dotnet copy-runtimes",
+                FullName = ".NET Core Native Runtimes Copier",
+                Description = "A tool to copy the native runtimes",
+            };
+            app.HelpOption("-h|--help");
+
+            var prefixOption = app.Option("-p|--prefix", "prefix of the internal packages", CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
+            {
+                var prefix = prefixOption.Value();
+
+                if (string.IsNullOrEmpty(prefix))
+                {
+                    app.ShowHelp();
+                    return 2;
+                }
+
+                Console.WriteLine("Copy the runtimes directory of internal package to current project");
+
+                var exitCode = new PrefixCommand(prefix).Run();
+
+                Console.WriteLine("Copying the runtimes completed successfully");
+
+                return exitCode;
+            });
+
+            try
+            {
+                return app.Execute(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ToString());
+            }
+
+            return 1;
         }
+
+        
     }
 }
